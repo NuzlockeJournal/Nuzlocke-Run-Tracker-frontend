b7705c89da845c6b5d84dd77117651fd2d29ff7f
David Richard <107142833+Dehrangerz9@users.noreply.github.com>
2024-09-12 14:50:36 -0300

    Innitial commit

---
+++ src/components/login/index.jsx
@@ -0,0 +1,12 @@
+import styles from "login.scss"
+
+function Login(){
+    //Laura crie aqui a tela de login :)
+    return(
+        <>
+        <p  className={styles.paragrafo}></p>
+        </>
+    )
+}
+
+export default Login
